3f0e63f647cd8c9bdad1c9df1a9c4e7aeaccf4a0
md-shakib-khan <mdshakibkhan.dev@gmail.com>
2025-04-15 10:26:21 +0600

    added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import FloatingBottomNavBar from "@/components/FloatingBottomBar";
 import type { Metadata } from "next";
 import "./globals.css";
-import FloatingBottomNavBar from "@/components/FloatingBottomBar";
 export const metadata: Metadata = {
   title: "Shakib Khan - Portfolio",
   description: "A showcase of my work and skills",
@@ -22,9 +22,44 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M88G9VB4');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZBRLNZQH5P"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZBRLNZQH5P');
+            `,
+          }}
+        />
+        {/* End Google tag (gtag.js) */}
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M88G9VB4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <FloatingBottomNavBar />
       </body>
